2bbb28053b2003607be20afca2a31d5b7198d4cc
Renata <renbabiuch@gmail.com>
2024-02-19 15:51:53 +0100

    add and refactor Page Objects

--- src/test/java/Page_Objects/SongLanguagesPage.java
+++ src/test/java/Page_Objects/SongLanguagesPage.java
@@ -1,15 +1,18 @@
 package Page_Objects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class SongLanguagesPage {
 
     private final WebDriver driver;
 
-    @FindBy(css = "button[data-test='close-exclude-languages']")
+    @FindBy(css = "[data-test='close-exclude-languages']")
     private WebElement continueButton;
 
     public SongLanguagesPage(WebDriver driver) {
@@ -19,5 +22,30 @@ public class SongLanguagesPage {
 
     public void continueAndGoToSongList() {
         continueButton.click();
+    }
+
+    public WebElement getLanguageElement(String language) {
+        return driver.findElement(By.cssSelector("[data-test=lang-" + language + "]"));
+    }
+
+    public WebElement getSongLanguageCheckbox(String language) {
+        return getLanguageElement(language).findElement(By.cssSelector("svg"));
+    }
+
+    public void ensureSongLanguageIsSelected(String language) {
+        if(getSongLanguageCheckbox(language).getAttribute("data-testid").equals("CheckBoxOutlineBlankIcon")) {
+            getSongLanguageCheckbox(language).click();
+        }
+    }
+
+    public void ensureAllSongLanguagesAreSelected() {
+
+        List<WebElement> languages = driver.findElements(By.cssSelector("[data-test^='lang-'] svg"));
+
+        for (WebElement language : languages) {
+            if (language.getAttribute("data-testid").equals("CheckBoxOutlineBlankIcon")) {
+                language.click();
+            }
+        }
     }
 }
--- src/test/java/Page_Objects/SongListPage.java
+++ src/test/java/Page_Objects/SongListPage.java
@@ -1,8 +1,10 @@
 package Page_Objects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
@@ -10,10 +12,12 @@ import java.util.List;
 public class SongListPage {
 
     private final WebDriver driver;
+    private Actions actions;
 
     public SongListPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+        actions = new Actions(driver);
     }
 
     public List<WebElement> getSongElement(String songID) {
@@ -22,6 +26,14 @@ public class SongListPage {
 
     public boolean isSongOnTheList(String songID) {
         return !getSongElement(songID).isEmpty();
+    }
+
+    public WebElement getSongPreviewElement() {
+        return driver.findElement(By.cssSelector("[data-test='song-preview']"));
+    }
+
+    public String getSelectedSongID() {
+        return getSongPreviewElement().getAttribute("data-song");
     }
 
     public WebElement getFullscreenElement() {
@@ -36,5 +48,9 @@ public class SongListPage {
     public boolean isFullscreenOn() {
         WebElement fullscreen = getFullscreenElement().findElement(By.cssSelector("[data-testid='FullscreenExitIcon']"));
         return fullscreen.isDisplayed();
+    }
+
+    public void goBackToMainMenu() {
+        actions.sendKeys(Keys.BACK_SPACE).perform();
     }
 }
